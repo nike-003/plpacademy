0dee81c410686c7575221d4490a533ffb7b47ea9
unknown <zakayomarry6@gmail.com>
2025-07-22 12:23:15 -0700

    Added Calculator File

---
+++ calculator.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum_result = num1 + num2
+product_result = num1 * num2
+quotient_result = num1 / num2
+difference_result = num1 - num2
+
+print("Sum:", sum_result)
+print("Product:", product_result)
+print("Quotient:", quotient_result)
+print("Difference:", difference_result)
